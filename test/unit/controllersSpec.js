ddfe6aeff068ccf7c2b74d94cfe64ccdfd7473e0
jeaveme <jeaveme@users.noreply.github.com>
2014-06-02 15:32:23 +0200

    Completa y mejora los tests unitarios de los controladores

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -45,8 +45,8 @@ describe('Controlador', function(){
 			ctrl = $controller('ListaCtrl', {$scope: scope});
 		}));
 
-		it("deberia cargar una lista 'incidencias' con 4 items", function() {
-			expect(scope.incidencias).toEqual(undefined);
+		it("debería cargar una lista 'incidencias' con 4 items", function() {
+			expect(scope.incidencias).toBeUndefined();
 			$httpBackend.flush();
 
 			expect(scope.incidencias.length).toEqual(4);
@@ -54,7 +54,7 @@ describe('Controlador', function(){
 	});
 
 	describe('VistaCtrl', function(){
-		var scope, ctrl, $httpBackend,
+		var scope, ctrl, location, $httpBackend,
 		incidencia = {
 			"_id": 1,
 			"titulo": "Integración con Git",
@@ -64,25 +64,40 @@ describe('Controlador', function(){
 			"descripcion": "Permitir poner un código en el mensaje del commit y que automáticamente se resuelva la incidencia"
 		};
 
-		beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+		beforeEach(inject(function(_$httpBackend_, $rootScope, $controller, $location) {
 			$httpBackend = _$httpBackend_;
-			$httpBackend.expectGET('/incidencias/1').
-			respond(incidencia);
-
+			$httpBackend.expectGET('/incidencias/1').respond(incidencia);
+			location = $location;
 			scope = $rootScope.$new();
-			ctrl = $controller('VistaCtrl', {$scope: scope, idIncidencia: 1});
+			ctrl = $controller('VistaCtrl', {$scope: scope, $location: location, idIncidencia: 1});
 		}));
 
-		it("deberia cargar un objeto con la incidencia", function() {
-			expect(scope.incidencia).toEqual(undefined);
+		it("debería cargar un objeto con la incidencia", function() {
+			expect(scope.incidencia).toBeUndefined();
 			$httpBackend.flush();
 
 			expect(scope.incidencia).toEqualData(incidencia);
+		});
+
+		it("debería eliminar la incidencia cuando se solicita", function() {
+			$httpBackend.flush();
+			expect(scope.incidencia).toBeTruthy();
+			$httpBackend.expectDELETE('/incidencias/1').respond("true");
+			scope.eliminar();
+			$httpBackend.flush();
+			expect(location.path()).toEqual('/');
+		});
+
+		it("debería abrir la incidencia en modo edición", function() {
+			$httpBackend.flush();
+			expect(scope.incidencia).toBeTruthy();
+			scope.modificar();
+			expect(location.path()).toEqual('/editar/1');
 		});
 	});
 
 	describe('EdicionCtrl', function(){
-		var scope, ctrl, $httpBackend,
+		var scope, ctrl, location, $httpBackend,
 		incidencia = {
 			"_id": 1,
 			"titulo": "Integración con Git",
@@ -92,33 +107,50 @@ describe('Controlador', function(){
 			"descripcion": "Permitir poner un código en el mensaje del commit y que automáticamente se resuelva la incidencia"
 		};
 
-		beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+		beforeEach(inject(function(_$httpBackend_, $rootScope, $controller, $location) {
 			$httpBackend = _$httpBackend_;
-			$httpBackend.expectGET('/incidencias/1').
-			respond(incidencia);
-
+			location = $location;
+			$httpBackend.expectGET('/incidencias/1').respond(incidencia);
 			scope = $rootScope.$new();
-			ctrl = $controller('EdicionCtrl', {$scope: scope, idIncidencia: 1});
+			ctrl = $controller('EdicionCtrl', {$scope: scope, $location: location, idIncidencia: 1});
 		}));
 
-		it("deberia cargar un objeto con la incidencia a editar", function() {
-			expect(scope.incidencia).toEqual(undefined);
+		it("debería guardar la edición de una incidencia", function() {
+			expect(scope.incidencia).toBeUndefined();
 			$httpBackend.flush();
-
 			expect(scope.incidencia).toEqualData(incidencia);
+			scope.incidencia.estado = "Resuelta";
+			$httpBackend.expectPOST('/incidencias/1', scope.incidencia).respond(scope.incidencia);
+			scope.aplicar();
+			$httpBackend.flush();
+			expect(location.path()).toEqual('/ver/1');
 		});
 	});
 
 	describe('NuevaCtrl', function(){
-		var scope, ctrl;
+		var scope, ctrl, httpBackend, location, nueva = {
+			"titulo": "Integración con SVN",
+			"tipo": "Mejora",
+			"estado": "Abierta",
+			"autor": "María",
+			"descripcion": "Permitir poner un código en el mensaje del commit y que automáticamente se resuelva la incidencia"
+		}, respuesta = Object.create(nueva);
+		respuesta._id = 100;
 
-		beforeEach(inject(function($rootScope, $controller) {
+		beforeEach(inject(function($rootScope, $controller, $httpBackend, $location) {
 			scope = $rootScope.$new();
+			httpBackend = $httpBackend;
+			location = $location;
 			ctrl = $controller('NuevaCtrl', {$scope: scope, idIncidencia: 1});
+			httpBackend.expectPOST('/incidencias', nueva).respond(respuesta);
 		}));
 
-		it("deberia crear un objeto con la incidencia abierta", function() {
+		it("debería crear un objeto con la incidencia abierta", function() {
 			expect(scope.incidencia).toEqualData({estado:"Abierta", tipo: "Bug"});
+			scope.incidencia = nueva;
+			scope.aplicar();
+			httpBackend.flush();
+			expect(location.path()).toEqual('/ver/100');
 		});
 	});
 });
